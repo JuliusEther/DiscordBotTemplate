356e414d9022e68021ce01af22b206f01890dedc
Julius Ether <julius.ether@gmail.com>
2021-07-21 01:35:14 +0900

    いったんWorkerの追加を取りやめ(Cog使ってやる方向で考える)

--- src/bot/core.py
+++ src/bot/core.py
@@ -3,7 +3,6 @@ from logging import Logger
 
 from discord.ext.commands import Bot
 
-from bot.worker import Worker
 from settings import Settings
 
 class Core:
@@ -15,17 +14,12 @@ class Core:
 	def Init(cls, logger:Logger):
 		cls._logger = logger
 		cls._bot = Bot(command_prefix=Settings.COMMAND_PREFIX)
-		
+
 		cls.Process()
 
 
 	@classmethod
 	def Process(cls):
-
 		loop = asyncio.get_event_loop()
-		futures = asyncio.gather(
-			Worker.Init(cls._bot, cls._logger)
-		)
 
-		loop.run_until_complete(futures)
-		
+
